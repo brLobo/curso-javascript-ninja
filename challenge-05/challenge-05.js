5ebedbf3892cb45932de1bc97ab2644893e41cc9
Bruno Lobo de Souza <lobodesouzabruno@gmail.com>
2020-05-01 12:29:35 -0300

    fix

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var a = [1,'1', true, {}, '2'];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myFunction(arg) {
+    return arg
+};
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(myFunction(a)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function myFArr(arr, ind) {
+    return arr[ind];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var b = [1,'1', true, null, undefined];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(myFArr(b,0));
+console.log(myFArr(b,1));
+console.log(myFArr(b,2));
+console.log(myFArr(b,3));
+console.log(myFArr(b,4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,51 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(nameBook) {
+    var allBooks = {
+        'name1': {
+            quantidadePaginas: 455,
+            autor: '01',
+            editora: 'lala'
+        },
+        'name2': {
+            quantidadePaginas: 544,
+            autor: '02',
+            editora: 'kaka'
+        },
+        'name3': {
+            quantidadePaginas: 895,
+            autor: '03',
+            editora: 'rara'
+        }
+    };
+
+    return !nameBook ? allBooks : allBooks[nameBook];
+
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log( 'O livro nome1 tem '+ book('name1').quantidadePaginas +' páginas!' );
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log( 'O autor do livro name2 é ' + book('name2').autor + '.' );
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log( 'O livro name3 foi publicado pela editora ' + book('name3').editora + '.' );
--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'catarinense';
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +13,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+var teams = ['Jec', 'Chapecoense', 'Figueirense', 'Avai', 'Criciuma'];
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -32,19 +33,32 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition(teamPosition) {
+    if (teamPosition > 4) {
+        return 'Não temos a informação do time que está nessa posição.'
+    };
+
+    return 'O time que está em '+ teamPosition + 'º lugar é o '+ teams[teamPosition] +'.';
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+console.log(showTeamPosition(0));
+console.log(showTeamPosition(1));
+console.log(showTeamPosition(2));
+console.log(showTeamPosition(5));
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+var counter = 20;
+
+while(counter <= 30) {
+    console.log(counter++);
+};
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +72,46 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+function convertToHex(color) {
+    var hexa = ''
+
+    switch (color) {
+        case 'black':
+            hexa = '000000'
+            break;
+        
+        case 'white':
+            hexa = 'FFFFFF'
+            break;
+        
+        case 'red':
+            hexa = 'FF0000'    
+            break;
+        
+        case 'green':
+            hexa = '00FF00'
+            break;
+        
+        case 'blue':
+            hexa = '0000FF'
+            break;
+    
+        default:
+            hexa = ''
+            //console.log('Não temos o equivalente hexadecimal para ' + color + '.')
+            break;
+    }    
+
+    return hexa ? console.log('O hexadecimal para a cor ' + color + ' é ' + hexa + '.')
+        : console.log('Não temos o equivalente hexadecimal para ' + color + '.')
+}
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+convertToHex('black');
+convertToHex('white');
+convertToHex('red');
+convertToHex('green');
+convertToHex('blue');
+convertToHex('aiusdhiuah');
